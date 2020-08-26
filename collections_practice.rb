acb07629818e6743e63dd5dfdf2b45ddd2bc9a47
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-26 23:25:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+array = [ 1,2,3,4,5]
 
+
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end 
+end
+
+def sort_array_desc(array)
+  array.sort! do |a,b| 
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |x|
+    
+  end
+end
+
+def find_a(array)
+  array.find_all do |x|
+    x[0] == "a"
+  end
+end
+
+
+
+   
+   
+
+
+  
